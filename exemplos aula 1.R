692dc18c2109ac24cb438350ece1018c9a0efc18
Jones Dhyemison Quito de Oliveira <jones-dqo@CGE.local>
2018-03-19 09:18:48 -0300

    first commit

---
+++ exemplos aula 1.R
@@ -0,0 +1,11 @@
+# CRIANDO UMA MATRIZ
+matriz = matrix(c(1:16),4,4)
+
+#Exibindo a matriz
+
+#Criando um vetor de letras
+letras = LETTERS[1:4]
+
+#Criando uma matriz 3 x 3 com letras de 'A' a 'F'
+matriz_char <- matrix(LETTERS[1:6],3,3)
+
