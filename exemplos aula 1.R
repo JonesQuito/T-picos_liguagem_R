2a05afe7e47c262512e6b6c72ce7460d6a076a50
Jones Dhyemison Quito de Oliveira <jones-dqo@CGE.local>
2018-03-19 10:09:58 -0300

    Criando uma matriz diagonal

--- exemplos aula 1.R
+++ exemplos aula 1.R
@@ -16,3 +16,11 @@ rownames(matriz) = c('São Paulo', 'Piracicaba', 'Americana', 'Madson')
 
 #Numerando as colunas de 1 a 4
 colnames(matriz) <- 1:4
+
+
+#Criando uma sequència de 10 a 100 com intervalo de 10
+sequencia = seq(10,100, by=10)
+
+#Criando uma matriz diagonal com 4 elementos na diagonal
+#começando em 10 e terminando em 40
+matriz_diagonal = diag(seq(10,40, by=10))
