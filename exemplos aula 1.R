155ff4820baae542071e148bfcc533ac034b274f
Jones Dhyemison Quito de Oliveira <jones-dqo@CGE.local>
2018-03-19 09:58:00 -0300

    nomes de colunas e linhas de matriz

--- exemplos aula 1.R
+++ exemplos aula 1.R
@@ -2,10 +2,16 @@
 matriz = matrix(c(1:16),4,4)
 
 #Exibindo a matriz
-
+print(matriz)
 #Criando um vetor de letras
 letras = LETTERS[1:4]
 
 #Criando uma matriz 3 x 3 com letras de 'A' a 'F'
 matriz_char <- matrix(LETTERS[1:6],3,3)
+print(matriz_char)
 
+#Criando nomes para as linhas de uma matriz
+rownames(matriz) = c('São Paulo', 'Piracicaba', 'Americana', 'Madson')
+
+#Numerando as colunas de 1 a 4
+colnames(matriz) <- 1:4
