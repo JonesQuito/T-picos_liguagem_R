581fc6f37d7881d0f4da328159a4eba9c15a843c
Jones Dhyemison Quito de Oliveira <jones-dqo@CGE.local>
2018-03-19 12:01:41 -0300

    matriz

--- exemplos aula 1.R
+++ exemplos aula 1.R
@@ -15,7 +15,7 @@ rownames(matriz) = c('São Paulo', 'Piracicaba', 'Americana', 'Madson')
 
 
 #Numerando as colunas de 1 a 4
-colnames(matriz) <- 1:4
+colnames(matriz) <- c('col-1', 'col-2', 'col-3', 'col-4')
 
 
 #Criando uma sequència de 10 a 100 com intervalo de 10
@@ -24,3 +24,33 @@ sequencia = seq(10,100, by=10)
 #Criando uma matriz diagonal com 4 elementos na diagonal
 #começando em 10 e terminando em 40
 matriz_diagonal = diag(seq(10,40, by=10))
+
+#-----------------------
+#Multiplicando matrizes
+matriz_produto = matriz %*% matriz_diagonal
+
+
+#Nomeando as colunas da matriz_produto
+colnames(matriz_produto) = c('col-1', 'col-2', 'col-3', 'col-4')
+
+
+#Cria uma matriz identidade de 4 elementos
+matriz_identidade = diag(4)
+
+
+#Acessando o elemento linha2 coluna2 da "matriz_diagonal"
+matriz_diagonal[2,2]
+
+
+#Acessando a linha 1 da "matriz"
+matriz[1,]
+
+
+#Acessando a coluna 3 da "matriz"
+matriz[,3]
+
+
+#Criando uma submatriz a partir de uma matriz já existente
+matriz[c(2:4), c(2:4)] #Acessando da linha 2 - 4 e coluna de 2 - 4
+
+matriz[-c(1, 3, 4), -c(1,4)]
