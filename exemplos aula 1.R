c801408eaea4283b9ce82e1933b74c5226ffffde
Jones <JonesQuito>
2018-03-20 00:12:46 -0300

    matrizes e dataframes mais arquivos de aula

--- exemplos aula 1.R
+++ exemplos aula 1.R
@@ -11,18 +11,18 @@ matriz_char <- matrix(LETTERS[1:6],3,3)
 
 
 #Criando nomes para as linhas de uma matriz
-rownames(matriz) = c('São Paulo', 'Piracicaba', 'Americana', 'Madson')
+rownames(matriz) = c('S?o Paulo', 'Piracicaba', 'Americana', 'Madson')
 
 
 #Numerando as colunas de 1 a 4
 colnames(matriz) <- c('col-1', 'col-2', 'col-3', 'col-4')
 
 
-#Criando uma sequència de 10 a 100 com intervalo de 10
+#Criando uma sequ?ncia de 10 a 100 com intervalo de 10
 sequencia = seq(10,100, by=10)
 
 #Criando uma matriz diagonal com 4 elementos na diagonal
-#começando em 10 e terminando em 40
+#come?ando em 10 e terminando em 40
 matriz_diagonal = diag(seq(10,40, by=10))
 
 #-----------------------
@@ -50,7 +50,25 @@ matriz[1,]
 matriz[,3]
 
 
-#Criando uma submatriz a partir de uma matriz já existente
+#Criando uma submatriz a partir de uma matriz j? existente
 matriz[c(2:4), c(2:4)] #Acessando da linha 2 - 4 e coluna de 2 - 4
 
 matriz[-c(1, 3, 4), -c(1,4)]
+
+matriz[c(T), c(F)]
+matriz[c(T, T, F, T), c(F, F, T, T)]
+
+#Obtendo um dataframe interno do R 'iris
+data("iris")
+
+#Retorna os elementos da coluna como um vetor
+coluna = iris$Sepal.Length
+
+
+#Obtem a quantidade de elementos de um vetor
+length(iris$Sepal.Length)
+
+
+
+
+
